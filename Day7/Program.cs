ea63b40306b117174c23ab55f7ff318428dc7fb1
Mikko Heikelä <mikko.heikela@futurice.com>
2018-12-09 17:14:09 +0000

    Day 7: refactoring + make the code print correct part 1 answer after changes made for part 2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -7,87 +7,61 @@ namespace Day7
 {
     class Program
     {
-        static void Main(string[] args)
+        static IEnumerable<(char job, int completesAt)> Simulate(
+            IEnumerable<(char prerequisite, char dependent)> adjacency,
+            int parallel = 1)
         {
-            //var lines = File.ReadLines("../../../sampleinput.txt");
-            var lines = File.ReadLines("../../../input.txt");
-            var adjacency = lines.Select(l => (first: l[5], second: l[36]));
-            var preqs = new Dictionary<char, HashSet<char>>();
+            var preqs = new Dictionary<char, HashSet<char>>(
+                adjacency.GroupBy(a => a.dependent)
+                    .Select(group => new KeyValuePair<char, HashSet<char>>(group.Key, new HashSet<char>(group.Select(a => a.prerequisite)))));
+
             var done = new HashSet<char>();
-            var remaining = new HashSet<char>(adjacency.SelectMany(a => new List<char>() { a.first, a.second}).Distinct());
-            foreach (var g in adjacency.GroupBy(a => a.second))
+            var remaining = new HashSet<char>(adjacency.SelectMany(a => new char[] { a.prerequisite, a.dependent }).Distinct());
+            var workerStates = new (int idx, int finishingTime, char task)[parallel];
+            for (int i = 0; i < parallel; ++i)
             {
-                preqs[g.Key] = new HashSet<char>(g.Select(a => a.first));
+                workerStates[i].idx = i;
             }
-            var workerFinishingTimes = new int[5];
+
             var time = 0;
-            var running = new SortedDictionary<char, int>();
             while (remaining.Count > 0)
             {
-                var available = remaining.Where(step =>
+                IEnumerable<char> availableTasks = remaining.Where(task => !preqs.ContainsKey(task) || preqs[task].All(preq => done.Contains(preq)));
+                Stack<int> availableWorkers = new Stack<int>(
+                    workerStates
+                        .Where(state => state.finishingTime <= time)
+                        .Select(state => state.idx));
+                while (availableTasks.Count() > 0 && availableWorkers.Count > 0)
                 {
-                    if (!preqs.ContainsKey(step))
-                    {
-                        return true;
-                    }
-                    foreach (var preq in preqs[step])
-                    {
-                        if (!done.Contains(preq))
-                        {
-                            return false;
-                        }
-                    }
-                    return true;
-                });
-                while (available.Count() > 0 && workerFinishingTimes.Min() <= time)
-                {
-                    var chosen = available.OrderBy(c => c).First();
-                    Console.Write("" + chosen);
-                    remaining.Remove(chosen);
-                    var worker = -1;
-                    for (int i = 0; i < 5; ++i)
-                    {
-                        if (workerFinishingTimes[i] <= time)
-                        {
-                            worker = i;
-                            break;
-                        }
-                    }
-                    var finishingTime = time + 61 + chosen - 'A';
-                    workerFinishingTimes[worker] = finishingTime;
-                    Console.WriteLine($"Worker {worker} starting task {chosen} at time {time}. It will finish at {finishingTime}");
-                    running.Add(chosen, finishingTime);
-                    available = available.Where(step => step != chosen);
+                    var chosenTask = availableTasks.OrderBy(c => c).First();
+                    remaining.Remove(chosenTask);
+                    availableTasks = availableTasks.Where(task => task != chosenTask);
+                    var chosenWorker = availableWorkers.Pop();
+                    int finishingTime = time + 61 + chosenTask - 'A';
+                    workerStates[chosenWorker].task = chosenTask;
+                    workerStates[chosenWorker].finishingTime = finishingTime;
+                    yield return (job: chosenTask, completesAt: finishingTime);
                 }
-
-/*                if (available.Count() > 0)
-                {*/
-                    time = running.Select(item => item.Value).Min();
-                    foreach (var step in running.Keys.ToList())
-                    {
-                        if (running[step] <= time)
-                        {
-                            running.Remove(step);
-                            done.Add(step);
-                        }
-                    }
-/*
-                }
-
-                var minFinishTime = workerFinishingTimes.Min();
-                if (minFinishTime > time)
+                int newTime = workerStates.Where(state => state.finishingTime > time).Select(state => state.finishingTime).Min();
+                foreach (var workerState in workerStates)
                 {
-                    time = minFinishTime;
-                    foreach (var step in running.Keys.ToList())
+                    if (workerState.finishingTime > time && workerState.finishingTime <= newTime)
                     {
-                        if (running[step] <= time)
-                        {
-                            running.Remove(step);
-                        }
+                        done.Add(workerState.task);
                     }
-                }*/
+                }
+                time = newTime;
             }
-            Console.WriteLine($"Part two, finishing time in parallel task running is: {workerFinishingTimes.Max()}");
+            yield break;
+        }
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("../../../input.txt");
+            var adjacency = lines.Select(l => (prerequisite: l[5], dependent: l[36]));
+            var sequentialSequence = Simulate(adjacency).Select(completion => completion.job).Aggregate("", (a, b) => a + b);
+            Console.WriteLine($"Tasks can be done in order {sequentialSequence}");
+            var parallelDuration = Simulate(adjacency, 5).Select(completion => completion.completesAt).Max();
+            Console.WriteLine($"Part two, finishing time in parallel task running is: {parallelDuration}");
         }
     }
 }
