32f4661228ff107b30de7bfa6673daf6bb822bbf
Mikko Heikelä <mikko.heikela@futurice.com>
2018-12-09 06:31:29 +0000

    Handle overflow and underflow in MarbleBuffer

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -4,67 +4,82 @@ using System.Diagnostics;
 
 namespace Day9
 {
-    class Program
+    class MarbleBuffer
     {
-
-        class MarbleBuffer {
-            private int[] arr;
-            private int head;
-            private int end;
-            public MarbleBuffer(int maxItems)
+        private int[] arr;
+        private int head;
+        private int end;
+        public int Count
+        {
+            get
             {
-                arr = new int[maxItems];
-                head = 0;
-                end = 0;
-            }
-            public void SkipForward()
-            {
-                if (head != end)
-                {
-                    arr[end] = arr[head];
-                    end = IncCursor(end);
-                    head = IncCursor(head);
-                }
-            }
-            public void SkipBackward()
-            {
-                if (head != end)
-                {
-                    end = DecCursor(end);
-                    head = DecCursor(head);
-                    arr[head] = arr[end];
-                }
-            }
-            public void Enqueue(int item)
-            {
-                arr[end] = item;
-                end = IncCursor(end);
-            }
-            public int Dequeue()
-            {
-                end = DecCursor(end);
-                return arr[end];
-            }
-            private int IncCursor(int cursor)
-            {
-                ++cursor;
-                if (cursor == arr.Length)
-                {
-                    cursor = 0;
-                }
-                return cursor;
-            }
-            private int DecCursor(int cursor)
-            {
-                --cursor;
-                if (cursor < 0)
-                {
-                    cursor = arr.Length - 1;
-                }
-                return cursor;
+                return (head - end) % arr.Length;
             }
         }
+        public MarbleBuffer(int maxItems)
+        {
+            arr = new int[maxItems];
+            head = 0;
+            end = 0;
+        }
+        public void SkipForward()
+        {
+            if (head != end)
+            {
+                arr[end] = arr[head];
+                end = IncCursor(end);
+                head = IncCursor(head);
+            }
+        }
+        public void SkipBackward()
+        {
+            if (head != end)
+            {
+                end = DecCursor(end);
+                head = DecCursor(head);
+                arr[head] = arr[end];
+            }
+        }
+        public void Enqueue(int item)
+        {
+            arr[end] = item;
+            end = IncCursor(end);
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Buffer out of space");
+            }
+        }
+        public int Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No item to dequeue");
+            }
+            end = DecCursor(end);
+            return arr[end];
+        }
+        private int IncCursor(int cursor)
+        {
+            ++cursor;
+            if (cursor == arr.Length)
+            {
+                cursor = 0;
+            }
+            return cursor;
+        }
+        private int DecCursor(int cursor)
+        {
+            --cursor;
+            if (cursor < 0)
+            {
+                cursor = arr.Length - 1;
+            }
+            return cursor;
+        }
+    }
 
+    class Program
+    {
         static long MarbleGame(int playerCount, int lastMarble)
         {
             long[] scores = new long[playerCount];
