c64132f6e67e0250d210d464a4bb03a0d6edd695
Mikko Heikelä <mikko.heikela@futurice.com>
2018-12-09 05:59:34 +0000

    Day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Day9
+{
+    class Program
+    {
+
+        class MarbleBuffer {
+            private int[] arr;
+            private int head;
+            private int end;
+            public MarbleBuffer(int maxItems)
+            {
+                arr = new int[maxItems];
+                head = 0;
+                end = 0;
+            }
+            public void SkipForward()
+            {
+                if (head != end)
+                {
+                    arr[end] = arr[head];
+                    end = IncCursor(end);
+                    head = IncCursor(head);
+                }
+            }
+            public void SkipBackward()
+            {
+                if (head != end)
+                {
+                    end = DecCursor(end);
+                    head = DecCursor(head);
+                    arr[head] = arr[end];
+                }
+            }
+            public void Enqueue(int item)
+            {
+                arr[end] = item;
+                end = IncCursor(end);
+            }
+            public int Dequeue()
+            {
+                end = DecCursor(end);
+                return arr[end];
+            }
+            private int IncCursor(int cursor)
+            {
+                ++cursor;
+                if (cursor == arr.Length)
+                {
+                    cursor = 0;
+                }
+                return cursor;
+            }
+            private int DecCursor(int cursor)
+            {
+                --cursor;
+                if (cursor < 0)
+                {
+                    cursor = arr.Length - 1;
+                }
+                return cursor;
+            }
+        }
+
+        static long MarbleGame(int playerCount, int lastMarble)
+        {
+            long[] scores = new long[playerCount];
+            MarbleBuffer marbles = new MarbleBuffer(lastMarble + 1);
+            marbles.Enqueue(0);
+            int currentMarble = 0;
+            int currentPlayer = 0;
+            while (currentMarble < lastMarble)
+            {
+                currentMarble++;
+                if (currentMarble % 23 != 0)
+                {
+                    marbles.SkipForward();
+                    marbles.Enqueue(currentMarble);
+                }
+                else
+                {
+                    scores[currentPlayer] += currentMarble;
+                    for (int i = 0; i < 7; ++i)
+                    {
+                        marbles.SkipBackward();
+                    }
+                    scores[currentPlayer] += marbles.Dequeue();
+                    marbles.SkipForward();
+                }
+                ++currentPlayer;
+                if (currentPlayer >= playerCount)
+                {
+                    currentPlayer = 0;
+                }
+            }
+            return scores.Max();
+        }
+        static void Main(string[] args)
+        {
+            Debug.Assert(MarbleGame(9, 25) == 32);
+            Debug.Assert(MarbleGame(10, 1618) == 8317);
+            Debug.Assert(MarbleGame(13, 7999) == 146373);
+            Debug.Assert(MarbleGame(17, 1104) == 2764);
+            Debug.Assert(MarbleGame(21, 6111) == 54718);
+            Debug.Assert(MarbleGame(30, 5807) == 37305);
+            Console.WriteLine($"Winning score is {MarbleGame(459, 71320)}");
+            Console.WriteLine($"Winning score is {MarbleGame(459, 71320 * 100)}");
+        }
+    }
+}
