4631acc5a229ef9679e6302f4b09ed15d7d8cc57
Mikko Heikelä <mikko.heikela@gmail.com>
2018-12-10 04:57:27 +0000

    Day 10 start

---
+++ Day10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("../../../input.txt");
+            Console.WriteLine($"Part one: ");
+        }
+    }
+}
