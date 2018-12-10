784cba4c4bd43e9538c7632bcd40264856013749
Mikko Heikelä <mikko.heikela@futurice.com>
2018-12-10 23:26:14 +0000

    A more fun approach: directly solve for a timespan where a random selection of the lights are closely aligned vertically, and print that period

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -54,6 +54,47 @@ namespace Day10
         }
     }
 
+    public struct Span
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public Span(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+        public static Span Inclusive(int a, int b)
+        {
+            if (a < b)
+            {
+                return new Span(a, b);
+            }
+            return new Span(b, a);
+        }
+        public static Span Inclusive(double a, double b)
+        {
+            if (a < b)
+            {
+                return new Span((int)Math.Floor(a), (int)Math.Ceiling(b));
+            }
+            return new Span((int)Math.Floor(b), (int)Math.Ceiling(a));
+        }
+        public static readonly Span Empty = new Span(int.MaxValue, int.MinValue);
+        public static readonly Span Maximal = new Span(int.MinValue, int.MaxValue);
+        public bool IsEmpty()
+        {
+            return Max < Min;
+        }
+        public bool Contains(int x)
+        {
+            return x >= Min && x <= Max;
+        }
+        public Span Intersect(Span other)
+        {
+            return new Span(Math.Max(Min, other.Min), Math.Min(Max, other.Max));
+        }
+    }
+
     class Program
     {
         static IEnumerable<Particle> Step(IEnumerable<Particle> prev, int dt = 1)
@@ -92,6 +133,12 @@ namespace Day10
             }
         }
 
+        /* solve for x in equation k1 * x + c1 = k2 * x + c2 */
+        static double Intercept(int k1, int c1, int k2, int c2)
+        {
+            return ((double)c2 - c1) / (k1 - k2);
+        }
+
         static void Main(string[] args)
         {
             var lines = File.ReadLines("../../../input.txt");
@@ -106,39 +153,64 @@ namespace Day10
                 return new Particle(new IntPoint2D(x, y), new IntPoint2D(vx, vy));
             }).ToList();
 
-            int time = 0;
-            int maxDuration = 100000;
-            IEnumerable<Particle> particles = initialParticles;
-            int step = 100;
-            int prevH = int.MaxValue;
-            int prevW = int.MaxValue;
-            while (time < maxDuration)
-            {
-                time += step;
-                particles = Step(particles, step);
-                var bounds = Bounds(particles);
-                var w = bounds.maxX - bounds.minX;
-                var h = bounds.maxY - bounds.minY;
-                Console.WriteLine($"At time {time}, the bounding box size of all particles is: {w} x {h}");
-                if (h > 600)
-                {
-                    step = 100;
-                } else if (h > 500)
-                {
-                    step = 5;
-                } else
-                {
-                    step = 1;
-                }
-                if (h > prevH || w > prevW || h < 15)
-                {
-                    OutputParticles(particles);
-                }
-                prevW = w;
-                prevH = h;
-            }
+            /* The examples shown look like the lettering is around 10 characters tall.
+             * It wasn't explicitly stated that that all given stars/particles participate in the pattern.
+             * As a heuristic based on these, I'll look for a time step when 10 randomly selected particles
+             * are within 15 Y coordinate steps of each other (pairwise) simultaneously */
 
-            Console.WriteLine($"Part one: ");
+            int particleCount = initialParticles.Count;
+            const int particlesToAlign = 10;
+            const int distanceThreshold = 15;
+            HashSet<int> randomIndices = new HashSet<int>();
+            var random = new Random();
+            while (randomIndices.Count < particlesToAlign)
+            {
+                randomIndices.Add(random.Next(particleCount));
+            }
+            var particlePairs = randomIndices.SelectMany(
+                i => randomIndices.SelectMany(
+                    j => {
+                        if (i == j)
+                        {
+                            return new List<(Particle, Particle)>();
+                        }
+                        return new List<(Particle, Particle)>() { (initialParticles[i], initialParticles[j])};
+                    })
+                );
+
+            var feasibleTimeSpans = particlePairs.Select(pair => {
+                if (pair.Item1.Vel.Y == pair.Item2.Vel.Y)
+                {
+                    if (Math.Abs(pair.Item1.Pos.Y - pair.Item2.Pos.Y) <= distanceThreshold)
+                    {
+                        return Span.Maximal;
+                    }
+                    else
+                    {
+                        return Span.Empty;
+                    }
+                }
+                else
+                {
+                    var t1 = Intercept(
+                        pair.Item1.Vel.Y, pair.Item1.Pos.Y - distanceThreshold,
+                        pair.Item2.Vel.Y, pair.Item2.Pos.Y);
+                    var t2 = Intercept(
+                        pair.Item1.Vel.Y, pair.Item1.Pos.Y + distanceThreshold,
+                        pair.Item2.Vel.Y, pair.Item2.Pos.Y);
+                    return Span.Inclusive(t1, t2);
+                }
+            });
+
+            var bestSpan = feasibleTimeSpans.Aggregate((a, b) => a.Intersect(b));
+
+            for (int time = bestSpan.Min; time <= bestSpan.Max; ++time)
+            {
+                var particles = Step(initialParticles, time);
+                Console.WriteLine($"At time {time}, we see:");
+                OutputParticles(particles);
+                Console.WriteLine();
+            }
         }
     }
 }
