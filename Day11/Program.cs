22f6ae0e76ab9480734788331a432c305859e0fb
Mikko Heikelä <mikko.heikela@gmail.com>
2018-12-11 04:59:59 +0000

    Day 11 start

---
+++ Day11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("../../../input.txt");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
