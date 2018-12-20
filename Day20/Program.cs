354167bb31050968f986af6cf2c99633b17d6283
Mikko Heikelä <mikko.heikela@gmail.com>
2018-12-20 04:59:23 +0000

    Day 20 start

---
+++ Day20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
